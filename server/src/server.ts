02ba160d6f972477bdd99242db464c96fdc35e63
Vinicius Gomes <vinicius.svgomes@gmail.com>
2023-01-03 22:30:49 -0300

    feat: server initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import '@config/env'
+
+import express from 'express'
+import cors from 'cors'
+
+async function bootstrap() {
+  const app = express()
+  const port = process.env.PORT || 3333
+
+  app.use(express.json())
+  app.use(cors())
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+  })
+}
+
+bootstrap()
