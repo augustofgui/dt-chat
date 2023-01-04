9e9a46abd3bc0180c334b67c13425d030b994182
Vinicius Gomes <vinicius.svgomes@gmail.com>
2023-01-03 22:55:14 -0300

    feat: add eslint settings

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,18 @@
-import '@config/env'
+import '@config/env';
 
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
 async function bootstrap() {
-  const app = express()
-  const port = process.env.PORT || 3333
+  const app = express();
+  const port = process.env.PORT || 3333;
 
-  app.use(express.json())
-  app.use(cors())
+  app.use(express.json());
+  app.use(cors());
 
   app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-  })
+    console.log(`Server running at http://localhost:${port}`);
+  });
 }
 
-bootstrap()
+bootstrap();
